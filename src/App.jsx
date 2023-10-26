d5f00fc92f7230d891d2fb596ebb8b7669859d47
Roy <jnmcluo@163.com>
2023-10-26 09:55:33 +0800

    模型载入

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import Left from "./components/Left";
-import ThreeD from "./components/ThreeD";
-import Learn from "./components/Learn";
+import {Model} from "./glbjsx/ivc";
+import {Canvas} from "@react-three/fiber";
+import {Suspense} from "react";
+import {OrbitControls, Stage, Html, useProgress} from "@react-three/drei";
+
+function Loader() {
+  const {progress} = useProgress();
+  return <Html center>{progress} % loaded</Html>;
+}
+
 function App() {
   return (
-    // <div className='w-screen h-screen flex'>
-    //   <div className='w-1/3 flex flex-col items-center justify-center bg-gray-900 text-white'>
-    //     <Left />
-    //   </div>
+    <div className='w-screen h-screen flex'>
+      <div className='w-1/3 flex flex-col items-center justify-center bg-gray-900 text-white'>
+        <Left />
+      </div>
 
-    //   <div className='flex-1'>
-    //     <ThreeD />
-    //   </div>
-    // </div>
-    <div className='w-screen h-screen'>
-      <Learn />
+      <div className='flex-1'>
+        <Canvas>
+          <Suspense fallback={<Loader />}>
+            <Stage>
+              <OrbitControls />
+              <Model />
+            </Stage>
+          </Suspense>
+        </Canvas>
+      </div>
     </div>
   );
 }
